6ade8a0e55ee652e94be5c443fb1c9c58f77b2bd
Rudyon <keremberket@gmail.com>
2022-08-22 22:54:26 +0300

    Implemented VGA text buffer

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-    let light_cyan_color = 0xb;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        // Add ascii bit to buffer
-        // Then add color bit to buffer
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = light_cyan_color;
-        }
-    }
+    vga_buffer::print_something();
 
     loop {}
 }
