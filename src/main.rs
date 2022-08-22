9a2e6c88eb97d1897586de600f8eb5e9bf6d0077
Auracle <Virgilfabre@outlook.com>
2022-08-22 16:22:50 -0400

    Revert "VGA text buffer"

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,23 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
-mod vga_buffer;
-
 use core::panic::PanicInfo;
 
 static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::write_string("Hello, Sekai!")
+    let vga_buffer = 0xb8000 as *mut u8;
+    let light_cyan_color = 0xb;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        // Add ascii bit to buffer
+        // Then add color bit to buffer
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = light_cyan_color;
+        }
+    }
 
     loop {}
 }
