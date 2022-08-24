e27f4c64ef46ed61e079574e2f8f9f0327a46e5e
Rudyon <keremberket@gmail.com>
2022-08-25 00:06:08 +0300

    fixed deadlock caused by timer interrupt handling

--- src/kernel.rs
+++ src/kernel.rs
@@ -19,7 +19,7 @@ pub extern "C" fn _start() -> ! {
     #[cfg(test)]
     test_main();
 
-    loop {}
+    veer::hlt_loop();
 }
 
 /// This function is called on panic.
@@ -27,7 +27,7 @@ pub extern "C" fn _start() -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop {}
+    veer::hlt_loop();
 }
 
 #[cfg(test)]
